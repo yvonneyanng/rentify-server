679d3dd538a1cb9ca5035c755cbf6297b7b52f17
Yvonne Yang <yvnyang@icloud.com>
2025-05-18 13:34:00 +1000

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5050;
 
 app.use(
   cors({
-    origin: "https://rentify-client-chi.vercel.app",
+    origin: ["https://rentify-client-chi.vercel.app", "http://localhost:5173/"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
